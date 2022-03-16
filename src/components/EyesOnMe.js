b264dba321bd7d9d86effd781c8a1d7f2a21bb6d
Allison <amh03160@gmail.com>
2022-03-15 19:49:11 -0500

    commit tree c380c212966976ab193180cef26a90efa49d9420

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(props){
+    function blur(){
+        console.log('Hey! Eyes on me!')
+    }
+    function focus(){
+        console.log('Good!')
+    }
+    return <button onFocus = {focus} onBlur = {blur} >Eyes on me</button>
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+
+function Keypad(props){
+    function enterPassword(){
+        console.log("Entering password...")
+    };
+    return <input type = "password" onChange = {enterPassword}/>
+};
+
+export default Keypad;
